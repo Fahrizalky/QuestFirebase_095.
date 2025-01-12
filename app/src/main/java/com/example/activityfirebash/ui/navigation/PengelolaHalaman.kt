0bd8b4332bcc32474fea910328a220d20cad12a4
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2025-01-12 23:44:29 +0700

    Membuat fungsi PengelolaHalaman dengan navigasi antar halaman.

---
+++ app/src/main/java/com/example/activityfirebash/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,40 @@
+package com.example.activityfirebash.ui.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.activityfirebash.ui.view.HomeScreen
+import com.example.activityfirebash.ui.view.InsertMhsView
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(navigateToltemEntry = {
+                navController.navigate(DestinasiInsert.route)
+            })
+
+        }
+
+        composable(DestinasiInsert.route) {
+            InsertMhsView(onBack = {
+                navController.popBackStack()
+            }, onNavigate = {
+                navController.navigate(DestinasiHome.route)
+            })
+        }
+    }
+}
