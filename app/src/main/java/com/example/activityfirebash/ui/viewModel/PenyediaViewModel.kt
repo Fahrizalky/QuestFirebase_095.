cac31ade45ff525c142c2d3d3c15334aa7571803
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2025-01-13 01:52:26 +0700

    Mendaftarkan viewModel di mahsiswa viewmodel

--- app/src/main/java/com/example/activityfirebash/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/activityfirebash/ui/viewModel/PenyediaViewModel.kt
@@ -1,18 +1,23 @@
 package com.example.activityfirebash.ui.viewModel
 
-import HomeViewModel
+
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.activityfirebash.MahasiswaAplication
+import com.example.activityfirebash.viewmodel.HomeViewModel
 
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+
         initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+
     }
 }
 
 fun CreationExtras.aplikasiMahasiswa(): MahasiswaAplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaAplication)
+
